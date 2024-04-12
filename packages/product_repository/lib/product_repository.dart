e9c4f18fdade9fb40ae7c6c57ddef63b4c9121dd
jpnsantoss <jpnsantos14@gmail.com>
2024-04-12 15:24:22 +0100

    product bloc

--- packages/product_repository/lib/product_repository.dart
+++ packages/product_repository/lib/product_repository.dart
@@ -1,8 +1,12 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library;
+library product_repository;
 
+export 'src/entities/entities.dart';
+export 'src/firebase_product_repo.dart';
+export 'src/models/models.dart';
 export 'src/product_repo.dart';
+
 
 // TODO: Export any libraries intended for clients of this package.
