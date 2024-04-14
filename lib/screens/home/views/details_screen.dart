acd622a2d401f3a9ef303a557efd7bacca9793b4
jpnsantoss <jpnsantos14@gmail.com>
2024-04-14 16:59:22 +0100

    some basic design

--- lib/screens/home/views/details_screen.dart
+++ lib/screens/home/views/details_screen.dart
@@ -13,22 +13,75 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Image.network(product.image),
-          const SizedBox(height: 16.0),
-          Text(
-            product.title,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24.0,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(product.image),
+              const SizedBox(height: 16.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    Text(
+                      product.location,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onTertiary,
+                      ),
+                    ),
+                    const SizedBox(height: 24.0),
+                    Text(product.user!.name),
+                    const SizedBox(height: 24.0),
+                    Text(
+                      product.description,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Theme.of(context).colorScheme.onTertiary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 48.0),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(20),
+                color: Theme.of(context).colorScheme.tertiary,
+                child: const Text(
+                  "Everything in this section is given away for free 💚. Strictly no selling, no swaps, no donations.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text("Request Product"),
             ),
           ),
-          Text(product.user!.name),
-          Text(product.location),
-          Text(product.description),
-          Text(product.location),
         ],
       ),
     );
