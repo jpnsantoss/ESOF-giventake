5b95fe75e01514560af604fd58075e2c39b65521
jpnsantoss <jpnsantos14@gmail.com>
2024-04-30 10:34:14 +0100

    fixed errors

--- lib/screens/profile/views/reviews_screen.dart
+++ lib/screens/profile/views/reviews_screen.dart
@@ -12,42 +12,56 @@ class ReviewsScreen extends StatefulWidget {
 class _ReviewsScreenState extends State<ReviewsScreen> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<GetReviewsBloc, GetReviewsState>(
-      builder: (context, state) {
-        return Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: widget.user.reviews.map((review) {
-              return Container(
-                margin: const EdgeInsets.symmetric(vertical: 5.0),
-                padding: const EdgeInsets.all(20.0),
-                width: 350,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5), // Cor da sombra
-                      spreadRadius: 3, // Raio de propagação da sombra
-                      blurRadius: 7, // Raio de desfoque da sombra
-                      offset: const Offset(0, 3), // Deslocamento da sombra
-                    ),
-                  ],
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Center(
+          child: BlocBuilder<GetReviewsBloc, GetReviewsState>(
+            builder: (context, state) {
+              if (state is GetReviewsSuccess) {
+                return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('User: ${review['username']}'),
-                    const SizedBox(height: 8), // Espaçamento entre as linhas
-                    Text('${review['review']}'),
-                  ],
-                ),
-              );
-            }).toList(),
+                  children: state.reviews.map((review) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(vertical: 5.0),
+                      padding: const EdgeInsets.all(20.0),
+                      width: 350,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            color:
+                                Colors.grey.withOpacity(0.5), // Cor da sombra
+                            spreadRadius: 3, // Raio de propagação da sombra
+                            blurRadius: 7, // Raio de desfoque da sombra
+                            offset:
+                                const Offset(0, 3), // Deslocamento da sombra
+                          ),
+                        ],
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        // children: [
+                        //   Text('User: ${review['username']}'),
+                        //   const SizedBox(
+                        //       height: 8), // Espaçamento entre as linhas
+                        //   Text('${review['review']}'),
+                        // ],
+                      ),
+                    );
+                  }).toList(),
+                );
+              } else if (state is GetReviewsFailure) {
+                return const Text('Failed to load reviews');
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
