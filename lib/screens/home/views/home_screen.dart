aae339d9972b3c7248571c0ccb7ce894b6204d8d
jpnsantoss <jpnsantos14@gmail.com>
2024-05-14 10:27:07 +0100

    header

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,14 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:giventake/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:giventake/screens/home/blocs/get_product_bloc/get_product_bloc.dart';
 import 'package:giventake/screens/home/views/details_screen.dart';
-import 'package:giventake/screens/home/views/edit_profile_screen.dart';
-import 'package:giventake/screens/product/views/upload_product_screen.dart';
-import 'package:user_repository/user_repository.dart';
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -24,66 +18,92 @@ class _HomeScreenState extends State<HomeScreen> {
     // Atualiza a lista de produtos sempre que a tela for exibida
     context.read<GetProductBloc>().add(GetProduct());
   }
+
   @override
   Widget build(BuildContext context) {
+    // final authBloc = BlocProvider.of<AuthenticationBloc>(context);
+    // String photoURL = authBloc.state.user?.photoURL ?? 'default_url';
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'GiveNTake',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 24,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w600,
+      appBar: AppBar(
+        title: const Text(
+          'GiveNTake',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Handle envelope icon tap
+            },
+            icon: const Icon(Icons.mail_outline), // Envelope symbol icon
+          ),
+          IconButton(
+            onPressed: () {
+              // Handle profile icon tap
+            },
+            icon: const CircleAvatar(
+              radius: 15,
+              // backgroundImage: NetworkImage(
+              //   photoURL,
+              // ), // Circle with profile image icon
             ),
           ),
-          actions: [
-            IconButton(
-              onPressed: () {
-                // Handle envelope icon tap
-              },
-              icon: Icon(Icons.mail_outline), // Envelope symbol icon
-            ),
-            IconButton(
-              onPressed: () {
-                // Handle profile icon tap
-              },
-              icon: CircleAvatar(
-                radius: 15,
-                backgroundImage: NetworkImage("https://via.placeholder.com/30x30"),
-              ), // Circle with profile image icon
-            ),
-          ],
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Give things another chance',
-                style: TextStyle(
-                  color: Color(0xFF6C8A47),
-                  fontSize: 100,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w600,
-                ),
+            Text(
+              "Give things\nanother chance",
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColor,
               ),
             ),
-            Card(
-              child: BlocBuilder<GetProductBloc, GetProductState>(
-                builder: (context, state) {
-                  return TextField(
-                    onChanged: (value) {
-                      context.read<GetProductBloc>().add(SearchProduct(value));
-                    },
-                    decoration: const InputDecoration(
-                      prefixIcon: Icon(CupertinoIcons.search),
-                      hintText: 'Search Items..',
+            const SizedBox(height: 20), // give it some space
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
-                  );
-                },
-              ),
+                    child: TextField(
+                      onChanged: (value) {
+                        context
+                            .read<GetProductBloc>()
+                            .add(SearchProduct(value));
+                      },
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(CupertinoIcons.search),
+                        hintText: 'Search Items..',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10), // give it some space
+                Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.add, color: Colors.white),
+                    onPressed: () {
+                      // Handle add button tap
+                    },
+                  ),
+                ),
+              ],
             ),
             Expanded(
               child: BlocBuilder<GetProductBloc, GetProductState>(
@@ -95,13 +115,14 @@ class _HomeScreenState extends State<HomeScreen> {
                         return ListTile(
                           title: Text(state.products[index].title),
                           subtitle: Text(state.products[index].description),
-                          leading: Image.network(state.products[index].image),
+                          //leading: Image.network(state.products[index].image),
                           onTap: () {
                             Navigator.push(
                               context,
                               MaterialPageRoute<void>(
                                 builder: (BuildContext context) =>
-                                    DetailsScreen(product: state.products[index]),
+                                    DetailsScreen(
+                                        product: state.products[index]),
                               ),
                             );
                           },
@@ -114,7 +135,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     );
                   } else {
                     return const Center(
-                      child: Text("An error has occurred..."),
+                      child: Text("An error has occured..."),
                     );
                   }
                 },
@@ -122,46 +143,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-
-  floatingActionButton: Padding(
-          padding: const EdgeInsets.only(left: 30.0, bottom: 16.0, right: 0.0),
-          child: Row(
-            mainAxisAlignment:
-                MainAxisAlignment.spaceBetween, // Alinha o botão à direita
-            children: [
-              FloatingActionButton(
-                heroTag: 'btn1',
-                onPressed: () async {
-                  final FirebaseAuth auth = FirebaseAuth.instance;
-                  final user = auth.currentUser;
-                  String userId = user!.uid;
-                  MyUser currentUser = await FirebaseUserRepo().getUser(userId);
-
-                  final result =await Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => EditProfileScreen(userId: userId, user: MyUserEntity(userId: userId, email: currentUser.email, name: currentUser.name, reviews: currentUser.reviews, rating: currentUser.rating, bio: currentUser.bio, image: currentUser.image,   )),
-          ));
-                },
-                child: const Icon(Icons.person),
-              ),
-              const Spacer(), // Espaçamento entre os botões
-              FloatingActionButton(
-                heroTag: 'btn2',
-                onPressed: () async {
-                  final result =
-                      await Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const ProductUploadScreen(),
-                  ));
-                  if (result == true) {
-                    // ignore: use_build_context_synchronously
-                    context.read<GetProductBloc>().add(GetProduct());
-                  }
-                },
-                child: const Icon(Icons.add),
-              ),
-            ],
-          ),
-
-        ));
+      ),
+    );
   }
 }
-      
